81e11363bdbd9b9182514294a7f3a1d50b91de37
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-19 13:49:11 -0700

    added comments for binary search printing problems

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
-# from dll_queue import Queue
-# from dll_stack import Stack
+from dll_queue import Queue
+from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -59,19 +59,55 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        # recursively
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+        #iteratively
+        # stack = Stack()
+        # stack.push(self)
+        # while len(stack) >0:
+        #     current_node = stack.pop()
+        #     if current_node.right:
+        #         stack.push(current_node.right)
+        #     if current_node.left:
+        #         stack.push(current_node.left)
+        #     cb(current_node.value)
+
+                
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
+        # make a stack
+        
+        # add root to stack
+        # while stack not empty then:
+        # pop it out (DO THE THING!!! IE PRINT)
+        # check left if exists add to stack, 
+        # check right if exists add to stack
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
+        # Make a queue
+        # put root in the queue
+        # while queue is not empty
+            # always pop out the thing at the top of the while loop
+            # pop out front of que
+            # (DO THE THING!!)
+            # If left:
+                # add left to back of queue
+            # if right:
+                # add right to back of queue
         pass
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
