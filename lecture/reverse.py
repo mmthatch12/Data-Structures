c3290f5d580a53b2da76c2e62f2b8dcc76524848
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-19 21:29:45 -0700

    reverse practice

--- lecture/reverse.py
+++ lecture/reverse.py
@@ -6,14 +6,65 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.size = 0
     def push(self, item):
         new_node = Node(item)
-        new_node.next = self.head
-        self.head = new_node
-        self.size += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+            self.size +=1
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
+    def printLL(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
 
 list1 = LinkedList()
+list1.push(1)
+list1.push(2)
+list1.push(3)
+list1.push(4)
 list1.push(5)
-list1.push(7)
-print(list1.size)
+list1.printLL()
+list1.reverse()
+list1.printLL()
+
+
+
+# def reverse(link):
+#     v1 = link.head
+#     v2 = link.head.next
+
+#     link.tail = v1
+
+#     while v2.next is not None:
+#         temp = v2.next
+#         v2.next = v1
+#         v1 = v2
+#         v2 = temp
+
+#     link.head = v2
+
+#     return link
+
+# l2 = reverse(list1)
+# cur = l2.head
+# while cur.next is not None:
+#     print(cur.value)
+
+    
